6cb7cd3ba121717dbf26df1b83d81621b610fb07
jellewass <168808094+jellewass@users.noreply.github.com>
2024-07-08 18:39:05 +0200

    Create vars.tf

---
+++ C1 - Azure Infrastructure Operations/project/terraform/vars.tf
@@ -0,0 +1,13 @@
+variable "prefix" {
+  description = "assignmentMachine"
+  default = "assignmentMachine"
+}
+
+variable "location" {
+  description = "East US"
+  default = "East US"
+}
+
+variable "resource_group" {
+  default = "Azuredevops"
+}
