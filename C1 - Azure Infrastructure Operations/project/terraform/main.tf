10c1e7164521176f278f39884339a4831a1efb96
jellewass <168808094+jellewass@users.noreply.github.com>
2024-07-08 18:52:03 +0200

    Update main.tf

--- C1 - Azure Infrastructure Operations/project/terraform/main.tf
+++ C1 - Azure Infrastructure Operations/project/terraform/main.tf
@@ -1,8 +1,9 @@
 provider "azurerm"{
   features {}
 }
-resource "azurerm_linux_virtual_machine" "example" {
-  name = "${var.prefix}-vm"
+
+resource "azurerm_virtual_network" "example" {
+  name = "${var.prefix}-vnet"
   resource_group_name = "${var.resource_group}"
   location = var.location
 
@@ -14,3 +15,46 @@ resource "azurerm_linux_virtual_machine" "example" {
     version = "latest"
   }
 }
+
+resource "azurerm_subnet" "internal" {
+  name = "internal"
+  resource_group_name = "${var.resource_group}"
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "example" {
+  name = "${var.prefix}-nic"
+  resource_group_name = "${var.resource_group}"
+  location = var.location
+
+  ip_configuration {
+    name = "internal"
+    subnet_id = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name = "var.prefix-vm"
+  resource_group_name = "${var.resource_group}"
+  location = var.location
+  size = "Standard_F2"
+  disable_password_authentication = true
+
+}
+
+source_image_reference {
+  publisher = "Canonical"
+  offer = "UbuntuServer"
+  sku = "20.04-LTS"
+  version = "latest"
+
+}
+
+os_disk {
+  storage_account_type = "Standard_LRS"
+  caching = "ReadWrite"
+}
+  
+
