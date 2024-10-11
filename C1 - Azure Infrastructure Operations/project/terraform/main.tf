377f77e40ffca9e06b30d832f384f19ac8a0756e
jellewass <168808094+jellewass@users.noreply.github.com>
2024-10-11 14:57:13 +0200

    Update main.tf

--- C1 - Azure Infrastructure Operations/project/terraform/main.tf
+++ C1 - Azure Infrastructure Operations/project/terraform/main.tf
@@ -11,7 +11,7 @@ resource "azurerm_virtual_network" "example" {
   source_image_reference {
     publisher = "Canonical"
     offer = "UbuntuServer"
-    sku = "20.04-LTS"
+    sku = "22.04-LTS"
     version = "latest"
   }
 }
@@ -47,7 +47,7 @@ resource "azurerm_linux_virtual_machine" "example" {
 source_image_reference {
   publisher = "Canonical"
   offer = "UbuntuServer"
-  sku = "20.04-LTS"
+  sku = "22.04-LTS"
   version = "latest"
 
 }
