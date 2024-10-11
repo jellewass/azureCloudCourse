49ff85d14bd7537b11d7cedd1ddd1200780f33d7
jellewass <168808094+jellewass@users.noreply.github.com>
2024-10-11 15:16:08 +0200

    Update main.tf

--- C1 - Azure Infrastructure Operations/project/terraform/main.tf
+++ C1 - Azure Infrastructure Operations/project/terraform/main.tf
@@ -40,6 +40,7 @@ resource "azurerm_linux_virtual_machine" "example" {
   network_interface_ids = [
     azurerm_network_interface.example.id,
 ]
+
   source_image_reference {
     publisher = "Canonical"
     offer = "UbuntuServer"
