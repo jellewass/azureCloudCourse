c152bc6690d75b3d0c19e6eb25084757b79debcb
jellewass <168808094+jellewass@users.noreply.github.com>
2024-10-11 17:40:24 +0200

    Update vars.tf

--- C1 - Azure Infrastructure Operations/project/terraform/vars.tf
+++ C1 - Azure Infrastructure Operations/project/terraform/vars.tf
@@ -11,3 +11,14 @@ variable "location" {
 variable "resource_group" {
   default = "Azuredevops"
 }
+
+variable "instance_count" {
+  description = "Number of instances to create"
+  type        = number
+  default     = 2
+
+  validation {
+    condition     = var.instance_count >= 2 && var.instance_count <= 5
+    error_message = "The instance_count must be between 2 and 5."
+  }
+}
